7674d00b03bdfc4759d5775ccf2d6f7489d45a1a
Nigel Harvey <Nashboy@users.noreply.github.com>
2022-04-22 20:52:58 -0400

    Built Functioning LocalStorage of post data and objects; NEXT: Sending and saving that data in applicable form

--- js/postBuild.js
+++ js/postBuild.js
@@ -1,4 +1,4 @@
-const blogTitleField = document.querySelector('.title');
+const postTitleField = document.querySelector('.title');
 const articleField = document.querySelector('.article');
 
 // // banner
@@ -49,3 +49,53 @@ const addImage = (imagepath, alt) => {
     let textToInsert = `\r![${alt}](${imagepath})\r`;
     articleField.value = articleField.value.slice(0, curPos) + textToInsert + articleField.value.slice(curPos);
 }
+
+
+// Post OBJECT
+function PostData(docName, date, title, article) {
+  let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  this.title = title;
+  this.article = article;
+  this.publishedAt = `${months[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`;
+  this.docName = docName;
+}
+
+
+
+publishBtn.addEventListener('click', () => {
+  makePost(articleField.value, postTitleField.value);
+})
+
+
+const makePost = (article, title) => {
+    if(!localStorage.getItem('posts')) {
+      localStorage.setItem('posts', "Empty")
+    }
+    console.log("POSTING....");
+    if(article.length && title.length){
+        // generating id
+        let letters = 'abcdefghijklmnopqrstuvwxyz';
+        let postTitle = title.split(" ").join("-");
+        let id = '';
+        for(let i = 0; i < 4; i++){
+            id += letters[Math.floor(Math.random() * letters.length)];
+        }
+
+        // setting up docName
+        let docName = `${postTitle}-${id}`;
+        let date = new Date(); // for published at info
+
+
+        let newPost = new PostData(docName, date, title, article);
+        console.log(newPost);
+        window.localStorage.setItem('posts', JSON.stringify(newPost));
+        let bruh = JSON.parse(window.localStorage.getItem('posts'))
+        console.log(`The title of this post is "${bruh.title}" with the content of "${bruh.article}" and it was published on ${bruh.publishedAt} under the name ${bruh.docName}`)
+        // .then(() => {
+        //     location.href = `/${docName}`;
+        // })
+        // .catch((err) => {
+        //     console.error(err);
+        // })
+    }
+  }
